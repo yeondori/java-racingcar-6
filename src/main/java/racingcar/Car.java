76d2dbcd5086aed2cb1adc3c573108f4062259f4
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 18:18:09 +0900

    feat: of, move 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,40 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final String name;
+    private int location;
+
+    private Car(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public static Car of(String name) {
+        validateName(name);
+        return new Car(name, 0);
+    }
+
+    private static void validateName(String name) {
+        if (name.length()>5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 5자 이내여야 합니다.");
+        }
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+        if (randomNumber >= 4) {
+            location++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
