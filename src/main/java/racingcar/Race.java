b41bf864bd5af6101cfab13b51fd9929fa3ef80e
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 19:15:55 +0900

    feat: rename(race -> run), 반환타입 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Race {
     private final List<Car> racingCars;
@@ -17,7 +21,11 @@ public class Race {
         return new Race(cars);
     }
 
-    public void race() {
-        racingCars.forEach(Car::move);
+    public Map<String, Integer> run() {
+        racingCars.forEach(
+                car -> car.move(Randoms.pickNumberInRange(0, 9)));
+
+        return racingCars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getPosition));
     }
 }
