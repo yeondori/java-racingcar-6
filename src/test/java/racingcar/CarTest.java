40ec524a37ede9231db8ea1edc30c559c0c7dfae
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 18:55:24 +0900

    feat: Car 생성 테스트 (이름에 대한 예외 처리)

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
     @Test
@@ -20,6 +21,18 @@ class CarTest {
         assertThat(toby.getName()).isEqualTo(carName);
     }
 
+    @Test
+    @DisplayName("이름이 5자 이상인 경우 예외를 던진다.")
+    void validateNameException() {
+        // given
+        String invalidName = "invalidName";
+
+        // when & then
+        assertThatThrownBy(() -> Car.of(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 5자 이내여야 합니다.");
+    }
+
     @DisplayName("4 미만인 경우에는 움직이지 않는다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
@@ -28,5 +41,15 @@ class CarTest {
         Car dori = Car.of("dori");
         dori.move(number);
         assertThat(dori.getPosition()).isZero();
+    }
+
+    @DisplayName("4 이상인 경우에는 전진한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void stopIfMoreThan4(int number) {
+
+        Car dori = Car.of("dori");
+        dori.move(number);
+        assertThat(dori.getPosition()).isEqualTo(1);
     }
 }
