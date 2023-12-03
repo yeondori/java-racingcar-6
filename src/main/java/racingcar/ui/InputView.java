df4f5aea268e421d368297d23304bb3d438c8116
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 19:56:50 +0900

    feat: 생성자, 입력 문구 추가

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    public InputView() {
+    }
+
     public String[] RequestCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
 
         validateCarsInput(input);
@@ -13,6 +17,7 @@ public class InputView {
     }
 
     public int RequestNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String input = readLine();
 
         validateNumberInput(input);
