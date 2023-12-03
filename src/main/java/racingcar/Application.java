52712628e47bba4f7678c473f2d6217aa923cedd
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 20:11:42 +0900

    feat: Racing 어플리케이션 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import racingcar.ui.InputView;
+import racingcar.ui.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] cars = inputView.RequestCars();
+        int roundNumber = inputView.RequestNumber();
+
+        Race race = Race.of(cars);
+        executeRacing(roundNumber, race);
+    }
+
+    private static void executeRacing(int roundNumber, Race race) {
+        System.out.println("실행 결과");
+
+        for (int round = 0; round < roundNumber; round++) {
+            Map<String, Integer> roundResult = race.run();
+            outputView.printResult(roundResult);
+        }
+
+        List<Car> winners = Result.getWinners(race.getRacingCars());
+        outputView.printWinners(winners);
     }
 }
